d7741d21228ca73d8acda41498ada110f27db6c1
Ivan <Aw4ken1ngs@gmail.com>
2021-10-07 18:06:18 +0300

    маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,14 @@
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 import MainPage from '../main-page/main-page';
+import Favorites from '../favorites/favorites';
+import FavoritesEmpty from '../favorites-empty/favorites-empty';
+import Login from '../login/login';
+import MainPageEmpty from '../main-page-empty/main-page-empty';
+import Offer from '../offer/offer';
+import Property from '../property/property';
+import PropertyNotLogged from '../property-not-logged/property-not-logged';
 
 type AppScreenProps = {
   cardCount: number;
@@ -6,7 +16,40 @@ type AppScreenProps = {
 
 function App({ cardCount }: AppScreenProps): JSX.Element {
   return (
-    <MainPage cardCount={cardCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainPage
+            cardCount={cardCount}
+          />
+        </Route>
+        <Route exact path={AppRoute.Favorites}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.FavoritesEmpty}>
+          <FavoritesEmpty />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <Login />
+        </Route>
+        <Route exact path={AppRoute.MainPageEmpty}>
+          <MainPageEmpty />
+        </Route>
+        <Route exact path={AppRoute.Room}>
+          <Offer />
+        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Property}
+          render={() => <Property />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
+        <Route exact path={AppRoute.PropertyNotLogged}>
+          <PropertyNotLogged />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
